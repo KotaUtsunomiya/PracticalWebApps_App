aa9ba2a48874b192779f92e5dfa860ca34755611
KotaUtsunomiya <r.ace.utsunomiya@gmail.com>
2022-11-25 14:51:03 +0900

    migration file add

---
+++ database/migrations/2022_11_25_052700_add_tag_id_todos_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddTagIdTodosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('todos', function (Blueprint $table) {
+            $table->unsignedBigInteger('tag_id')->index();
+            $table->foreign('tag_id')
+                ->references('id')->on('tags')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('todos', function (Blueprint $table) {
+            $table->dropForeign('todos_tag_id_foreign');
+        });
+    }
+}
